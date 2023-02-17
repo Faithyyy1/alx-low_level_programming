5fc8ce0b396d0b14f43d4335d8cf11fd9803cb46
Faithyyy1 <ajibolamyrabel@gmail.com>
2023-02-16 16:53:44 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ *main - Write a C program that prints exactly hello,puts
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
