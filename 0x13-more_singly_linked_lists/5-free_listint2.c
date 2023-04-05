0be0317be108bad84d558e25feeb345948079fa9
Faithyyy1 <ajibolamyrabel@gmail.com>
2023-04-05 16:37:50 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "lists.h"
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current_node;
+	while (*head != NULL)
+	{
+		current_node = *head;
+		*head = (*head)->next;
+		free(current_node);
+	}
+	*head = NULL;
+}
+
